5ed37a7e0aa5ea219552e1a3b13655f8716c747a
Optimalsten <soa@prg.spb.ru>
2023-09-05 00:39:37 +0300

    feat: add input in project HOMEWORK 002 of SEMINAR 1

--- SEM1_HOMEWORK002/Program.cs
+++ SEM1_HOMEWORK002/Program.cs
@@ -3,3 +3,33 @@
 // 2   3   7   -> 7
 // 44  5   78  -> 78
 // 22  3   9   -> 22
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+Console.WriteLine("\nЗадайте три целых числа для поиска максимального из них.");
+
+Console.Write("\nВведите первое число: ");
+string strNum = Console.ReadLine();
+int num1 = int.Parse(strNum);
+
+Console.Write("Введите второе число: ");
+strNum = Console.ReadLine();
+int num2 = int.Parse(strNum);
+
+Console.Write("Введите третье число: ");
+strNum = Console.ReadLine();
+int num3 = int.Parse(strNum);
+
+
+
+
+
+
+Console.Write("\nДля продолжения нажмите любую клавишу..."); //  "\n - "возврат каретки"
+Console.Read(); // Задержка экрана
