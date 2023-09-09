988e60a52e9dbcb0296162f0ba1286b27eb5f456
Optimalsten <soa@prg.spb.ru>
2023-09-09 16:17:49 +0300

    feat: add to project Solution from GB - HOMEWORK 001_00 Seminar 3

---
+++ SEM3_HOMEWORK001_00/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 19: Напишите программу, которая
+// принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+// Внутри класса Answer напишите метод IsPalindrome,
+// который принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+// Метод должен проверить является ли число пятизначным,
+// в противном случае - вывести Число не пятизначное и False в следующей строке.
+// Для остальных чисел вернуть True или False.
+// 14212 -> False
+// 12821 -> True
+// 234322 -> Число не пятизначное
+//                      False
+using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+        if (number < 10000 || number >= 100000){
+            Console.WriteLine("Число не пятизначное");
+            return false;
+        }
+        // СУПЕР - ПОЛУЧЕНИЕ "ОБРАТНОГО" ЧИСЛА (независимо от количества разрядов) !!!
+        int reverse = 0;
+        int temp = number;
+        while (temp > 0){
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        return reverse == number;
+    }
+
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+            number = 13731;
+        }
+
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
