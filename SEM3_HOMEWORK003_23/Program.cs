633073e0269274f4bf25e854acef246aad29a215
Optimalsten <soa@prg.spb.ru>
2023-09-09 10:26:57 +0300

    feat: add to project programm text HOMEWORK 003_23 of SEMINAR 3

--- SEM3_HOMEWORK003_23/Program.cs
+++ SEM3_HOMEWORK003_23/Program.cs
@@ -4,3 +4,28 @@
 // 3 -> 1, 4, 9.
 // 5 -> 1, 8, 27, 64, 125.
 
+Console.WriteLine("\nЗадайте целое положительное число (N)" +
+"\nдля определения кубов чисел от 1 до N.\n");
+Console.Write($"Введите число N : ");
+
+string numbStr = Console.ReadLine();
+int num = int.Parse(numbStr);
+
+if (num < 1)
+{
+    Console.Write($"\nЗаданное Вами число ({num}) - некорректно.");
+}
+else
+{
+    Console.Write($"\nЧисло     Куб числа\n");
+    int numN = 1;
+    while (numN <= num)
+    {
+        Console.Write($"\n  {numN}           {numN * numN * numN}");
+        numN++;
+    }
+}
+// Задержка экрана
+Console.WriteLine("\n\nДля продолжения нажмите любую клавишу..."); //  "\n - "возврат каретки"
+Console.Read();
+
