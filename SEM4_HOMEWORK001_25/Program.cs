588ac2d11d14f83313e8567fdb3c2d0d4cf452ac
Optimalsten <soa@prg.spb.ru>
2023-09-13 02:30:05 +0300

    feat: add to project HOMEWORK 001_25 SEMINAR 4

---
+++ SEM4_HOMEWORK001_25/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+string GetStr(string message)
+{
+    Console.Write($"Введите число {message}");
+    string numberStr = Console.ReadLine();
+    return numberStr;
+}
+
+Console.WriteLine("\nЗадайте два числа (А, В)" +
+"\nдля возведения числа А в натуральную степень В.\n");
+
+string numStrA = GetStr("A, которое будет возводиться в степень. А = ");
+string numStrB = GetStr($"B (натуральное) - степень, в которую будет возводиться число {numStrA}. B = ");
+
+bool contrA = double.TryParse(numStrA, out double numA);
+bool contrB = int.TryParse(numStrB, out int numB);
+
+bool control = contrA && contrB && numB > 0;
+
+switch (control.ToString()) // проверка допустимых значений А и В
+{
+    case "False":     // одно или оба значения (заданные для А и В) - некорректны
+        if (!contrA) // в numStrA - не действительное число, numA == 0
+        {
+            Console.WriteLine($"\nВведенное значение A некорректно.");
+        }
+        if (!contrB || numB <= 0) // в numStrB - ненатуральное число, numB == 0
+        {
+            Console.WriteLine($"\nВведенное значение B некорректно");
+        }
+        break;
+
+    case "True":     // оба значения (заданные для А и В) - корректны
+        double result = Math.Pow(numA, numB);
+        Console.WriteLine($"\nРезультат равен {result:F3}.");
+        break;
+}
+
+// Задержка экрана
+Console.WriteLine("\nДля продолжения нажмите любую клавишу..."); //  "\n - "возврат каретки"
+Console.Read();
