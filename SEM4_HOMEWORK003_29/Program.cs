3176578575558b5a729c71ca3415de0e8dc5497b
Optimalsten <soa@prg.spb.ru>
2023-09-13 22:07:35 +0300

    feat: add to project programm HOMEWORK 003(29) SEMINAR 3

---
+++ SEM4_HOMEWORK003_29/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+bool GetNumArr(int size, out int[] arrayNN) // ввод элементов массива, конверт.(контроль допустим.)
+{
+    int[] arrayNN = new int[size];
+    bool contrNum = true;
+    
+    Console.WriteLine($"\nЗадайте массив NN[] из {size} элементов (целых чисел)" +
+    "\n для последующего вывода массива на экран.");
+    
+    for (int i = 0; i < size;  i++)
+    {
+        Console.WriteLine($"Задайте значение {i}-го элемента массива. NN[{i}] = ");
+        string numberStr = Console.ReadLine();
+
+        //contrNum = int.TryParse(numberStr, out int numN);
+        contrNum = contrNum & int.TryParse(numberStr, out int numN);
+        arrayNN[i] = numN; // В случае некорректного ввода array[i] будет = 0
+    }
+    return contrNum;
+}
+
+void Print(int[]arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int sizeArr = 8;
+int[] arrayNN = new int[sizeArr];
+bool contrN = GetNumArr(sizeArr, out int[] arrayNN);
+if (!contrN)
+{
+    Console.WriteLine($"\nНе все заданные элементы массива целых чисел корректны.");
+}
+else
+{
+    Print(arrayNN);
+}
+
+// Задержка экрана
+Console.WriteLine("\nДля продолжения нажмите любую клавишу..."); //  "\n - "возврат каретки"
+Console.Read();
